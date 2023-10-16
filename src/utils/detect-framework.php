d731408e3ece2bc93d0ea207c0551ebf352bcd16
rendra <oii.r3ndr@gmail.com>
2023-10-17 02:00:11 +0700

    modifikasi composer.json

--- src/utils/detect-framework.php
+++ src/utils/detect-framework.php
@@ -1,23 +1,32 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Mendeteksi Laravel
 if (file_exists(getcwd() . '/artisan')) {
     // Ini adalah Laravel
     // Tambahkan provider ke app.php
-    $configApp = file_get_contents(getcwd() . '/config/app.php');
-    if (!strpos($configApp, 'SDKlrvlSSOServiceProvider::class')) {
-        $providerToAdd = "\n        App\\Providers\\SDKlrvlSSOServiceProvider::class,";
-        $configApp = str_replace("'providers' => [", "'providers' => [" . $providerToAdd, $configApp);
-        file_put_contents(getcwd() . '/config/app.php', $configApp);
-    }
-    
-    // Salin sdksksso.php ke folder config
-    if (!file_exists(getcwd() . '/app/Providers/SDKlrvlSSOServiceProvider.php')) {
-        copy('src/utils/source/laravel/Providers/SDKlrvlSSOServiceProvider.php', getcwd() . '/app/Providers/SDKlrvlSSOServiceProvider.php');
-    }
+    try
+    {
+        $configApp = file_get_contents(getcwd() . '/config/app.php');
+        if (!strpos($configApp, 'SDKlrvlSSOServiceProvider::class')) {
+            $providerToAdd = "\n        App\\Providers\\SDKlrvlSSOServiceProvider::class,";
+            $configApp = str_replace("'providers' => [", "'providers' => [" . $providerToAdd, $configApp);
+            file_put_contents(getcwd() . '/config/app.php', $configApp);
+        }
+        
+        // Salin sdksksso.php ke folder config
+        if (!file_exists(getcwd() . '/app/Providers/SDKlrvlSSOServiceProvider.php')) {
+            copy(__DIR__ . '/source/laravel/Providers/SDKlrvlSSOServiceProvider.php', getcwd() . '/app/Providers/SDKlrvlSSOServiceProvider.php');
+        }
 
-    // Salin sdksksso.php ke folder config
-    if (!file_exists(getcwd() . '/config/sdksksso.php')) {
-        copy('src/utils/config/sdksksso.php', getcwd() . '/config/sdksksso.php');
+        // Salin sdksksso.php ke folder config
+        if (!file_exists(getcwd() . '/config/sdksksso.php')) {
+            copy(__DIR__ . '/config/sdksksso.php', getcwd() . '/config/sdksksso.php');
+        }
+    } catch (Exception $e) {
+        // Anda bisa memutuskan untuk menampilkan pesan kesalahan atau log kesalahan.
+        echo "Terjadi kesalahan saat menulis ke config/app.php: " . $e->getMessage();
     }
 }
 // Mendeteksi CodeIgniter
